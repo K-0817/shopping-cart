500aff20de07f0857fb7a6b03d9a9d6d63509f65
Full Stack Developer <102760454+K-0817@users.noreply.github.com>
2022-07-16 21:26:05 +0200

    update

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react';
 import * as ReactDOMClient from 'react-dom/client';
 
 /* Theme */
+
+
 import { ThemeProvider } from 'commons/style/styled-components';
 import { theme } from 'commons/style/theme';
 import GlobalStyle from 'commons/style/global-style';
